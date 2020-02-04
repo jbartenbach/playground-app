9c767c1b69bce0dcd66bb26f240aee4afe92b539
jeff <jbartenbach@gmail.com>
2020-02-04 04:50:21 -0500

    added css. components are still hardcoded

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+let stories = [
+  {label: "welcome", title: "Short Stories the fun way", author: "Jeff Bartenbach", shortbody: "Pointed Stories is a new way to enjoy short stories. It’s quite simply a feed of the best stories, from the classics to contemporary writers."},
+  {label: "Short story", title: "A Great Title for the Ages", author: "John Doe", shortbody:"This is an example of a card without an image. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec viverra odio."}
+]
+
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div id="main">
+    <div class="logo2"></div>
+      <div class="card">
+        <div class="card-image"></div>
+        <div class="card-text-container">
+          <div class="card-label">Welcome</div>
+          <div class="card-title">Short stories the fun way</div>
+          <div class="card-author"></div>
+          <div class="card-short-body">Pointed Stories is a new way to enjoy short stories. It’s quite simply a feed of the best stories, from the classics to contemporary writers.</div>
+        </div>
+      </div>
+      <div class="card">
+          <div class="card-no-image"></div>
+          <div class="card-text-container">
+            <div class="card-label">Short Story</div>
+            <div class="card-title">A Great Title for the Ages</div>
+            <div class="card-author">By John Doe</div>
+            <div class="card-short-body">This is an example of a card without an image. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec viverra odio.</div>
+          </div>
+      </div>
     </div>
+
   );
-}
+};
 
 export default App;
